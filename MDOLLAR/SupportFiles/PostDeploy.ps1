235b5dbe3590477a93e49288576f4eb5d12614c0
rajeshbsMS <45964568+rajeshbsMS@users.noreply.github.com>
2019-03-27 22:09:50 +0530

    Update PostDeploy for MDOLLAR

--- MDOLLAR/SupportFiles/PostDeploy.ps1
+++ MDOLLAR/SupportFiles/PostDeploy.ps1
@@ -20,23 +20,26 @@ param
 $logfilename="C:\WindowsAzure\Logs\TWSCustomInstallLog.txt"
 Write-Output $message | Out-File -FilePath $logfilename -Append -Force
 }
+
+
 $WebClient = New-Object System.Net.WebClient
 Write-FileLog("Downloading SupportFiles.Zip...")
 #RoboCopy "\\BAYTWSSQLWAW101\ScriptsandExecutables" C:\Packages\Plugins SupportFiles.zip /XN
 $WebClient.DownloadFile($SupportFilesURL,"C:\Packages\Plugins\Supportfiles.zip")
 
-Sleep 5
+Sleep 10
 Write-FileLog("Extracting SupportFiles.Zip to C:\Packages\Plugins...")
 Expand-Archive C:\Packages\Plugins\SupportFiles.zip -DestinationPath C:\Packages\Plugins\SupportFiles
 
-Write-FileLog("Converting $UserName and password to Credential...")
-$pass = ConvertTo-SecureString -AsPlainText $Password -Force
+#Write-FileLog("Converting $UserName and password to Credential...")
+#$pass = ConvertTo-SecureString -AsPlainText $Password -Force
 
-$SecureString = $pass
+#$SecureString = $pass
 # Users you password securly
-$MySecureCreds = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $Username,$SecureString
+#$MySecureCreds = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $Username,$SecureString
 
 Write-FileLog("Calling AppConfiguration.ps1...")
-Invoke-Command -ComputerName $env:COMPUTERNAME -Credential $MySecureCreds -FilePath 'C:\Packages\Plugins\SUpportFiles\AppConfiguration.ps1' -ArgumentList $Role,$xpertenvName,$xpertRole,$xpertServiceKey
+#Invoke-Command -ComputerName $env:COMPUTERNAME -Credential $MySecureCreds -FilePath 'C:\Packages\Plugins\SUpportFiles\AppConfiguration.ps1' -ArgumentList $Role,$xpertenvName,$xpertRole,$xpertServiceKey
+Invoke-Command -ComputerName $env:COMPUTERNAME -FilePath 'C:\Packages\Plugins\SUpportFiles\AppConfiguration.ps1' -ArgumentList $Role,$xpertenvName,$xpertRole,$xpertServiceKey
 Write-FileLog("Completed Post Deploy Activities...")
 
