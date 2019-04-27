d669ff2340dc0cdd79116c672ef8b3012fae94e7
rajeshbsMS <45964568+rajeshbsMS@users.noreply.github.com>
2019-04-27 23:13:17 +0530

    Update PostDeploy.ps1

--- MDOLLAR/SupportFiles/PostDeploy.ps1
+++ MDOLLAR/SupportFiles/PostDeploy.ps1
@@ -35,17 +35,21 @@ Write-FileLog("Converting $UserName and password to Credential...")
 $pass = ConvertTo-SecureString -AsPlainText $Password -Force
 
 $SecureString = $pass
+$username=$env:COMPUTERNAME+"\"+$Username
 # Users you password securly
 $MySecureCreds = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $Username,$SecureString
 
-Write-FileLog("Calling AppConfiguration.ps1...")
-#Invoke-Command -ComputerName $env:COMPUTERNAME -Credential $MySecureCreds -FilePath 'C:\Packages\Plugins\SUpportFiles\AppConfiguration.ps1' -ArgumentList $Role,$xpertenvName,$xpertRole,$xpertServiceKey
-#Invoke-Command -ComputerName $env:COMPUTERNAME -FilePath 'C:\Packages\Plugins\SUpportFiles\AppConfiguration.ps1' -ArgumentList $Role,$xpertenvName,$xpertRole,$xpertServiceKey
-&'C:\Packages\Plugins\SUpportFiles\AppConfiguration.ps1' -ArgumentList SQL,TESTENVNAME,TESTROLE,TESTSERVICEKEY
+
 $Logininfo="Executing as " + $env:UserDomain + "\" + $env:UserName + " on " + $env:ComputerName
 Write-Host $Logininfo
 Write-FileLog -message $Logininfo
 Write-FileLog("Calling AppConfiguration in context of $env:UserName")
-Write-FileLog("Invoke-Command -ComputerName $env:COMPUTERNAME -FilePath 'C:\Packages\Plugins\SUpportFiles\AppConfiguration.ps1' -ArgumentList $Role,$xpertenvName,$xpertRole,$xpertServiceKey")
-Write-FileLog("Completed Post Deploy Activities...")
+Write-FileLog("Invoke-Command -ComputerName $env:COMPUTERNAME -Credential $MySecureCreds -FilePath 'C:\Packages\Plugins\SUpportFiles\AppConfiguration.ps1' -ArgumentList $Role,$xpertenvName,$xpertRole,$xpertServiceKey")
 
+
+
+Invoke-Command -ComputerName $env:COMPUTERNAME -Credential $MySecureCreds -FilePath 'C:\Packages\Plugins\SUpportFiles\AppConfiguration.ps1' -ArgumentList $Role,$xpertenvName,$xpertRole,$xpertServiceKey
+#Invoke-Command -ComputerName $env:COMPUTERNAME -FilePath 'C:\Packages\Plugins\SUpportFiles\AppConfiguration.ps1' -ArgumentList $Role,$xpertenvName,$xpertRole,$xpertServiceKey
+
+
+Write-FileLog("Completed Post Deploy Activities...")
