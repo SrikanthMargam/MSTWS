7cc1e63132c0c848baed9a3c5b4f69668f4b41bc
ajayvel <45480108+ajayvel@users.noreply.github.com>
2019-07-23 15:29:57 +0530

    Updated for Domains
    Updated the Domains

--- TWSInfra/ClientTools/SpinVM.ps1
+++ TWSInfra/ClientTools/SpinVM.ps1
@@ -1,7 +1,7 @@
 ﻿
 #Template Location
-$sqlTemplate="https://raw.githubusercontent.com/MSTWS/TWSArm/master/TWSInfra/Templates/sql-vm-deploy.json"
-$webTemplate="https://raw.githubusercontent.com/MSTWS/TWSArm/master/TWSInfra/Templates/web-vm-deploy.json"
+$sqlTemplate = "https://raw.githubusercontent.com/MSTWS/TWSArm/master/TWSInfra/Templates/sql-vm-deploy.json"
+$webTemplate = "https://raw.githubusercontent.com/MSTWS/TWSArm/master/TWSInfra/Templates/web-vm-deploy.json"
 #$webTemplate="https://raw.githubusercontent.com/MSTWS/TWSArm/master/TWSInfra/Templates/web-vm-deploy.json"
 
 #PARAMETER HELP
@@ -19,280 +19,236 @@ $webTemplate="https://raw.githubusercontent.com/MSTWS/TWSArm/master/TWSInfra/Tem
 #MENTION SIZE IN GB - In this fashion: 32, 64, 128, 256, 512, 1024,2048,4095 
 
 # BEGIN PARAMETERS
-$Role="WEB"
-$SubRole="WEB" 
-$DC="POC" 
-$StackCode="SRS"
-$StartCounter=302
-$NumberofServers=1
-$CDrive=256
-$DDrive=128
-$EDrive=512
-$SKU="Standard_DS12_v2" 
-$StorageType="Standard_LRS"
-$DomainAccountName="phx\rajeshbs"
+$Role = "WEB"
+$SubRole = "WEB" 
+$DC = "POC" 
+$StackCode = "SRS"
+$StartCounter = 101
+$NumberofServers = 1
+$CDrive = 256
+$DDrive = 128
+$EDrive = 512
+$SKU = "Standard_DS12_v2" 
+$StorageType = "Standard_LRS"
+$DomainAccountName = "pme\ajayvel"
 $DomainPassword = Read-Host -Prompt "Enter your DOMAIN Password for $DomainAccountName" -AsSecureString
 
-
-
 #MENTION ONLY IF SQL SERVERS ELSE IGNORE
-$HDrive=1024
-$ODrive=256
-$TDrive=256 
+$HDrive = 1024
+$ODrive = 256
+$TDrive = 256 
 
 #DO NOT CHANGE THE BELOW VALUES. CONTACT RAJESHBS OR AJAYVEL FOR USING THE BELOW PARAMETERS.
-$ServerName="BN2TWSFILOCO101"
-$OverrideNamingRules=$false
+$ServerName = "BN2TWSFILOCO101"
+$OverrideNamingRules = $false
 #END OF PARAMETERS
 
 
 #AUTOGENERATION 
-$Domaintojoin="PHX.GBL"
-$OUPath="OU=TWS-Services,OU=Resource,OU=Production,DC=phx,DC=gbl"
-$LocalAdminName="twsadmin"
-$LocalAdminSecuredPassword = "01000000d08c9ddf0115d1118c7a00c04fc297eb010000005c615ee69dea6b45a4c6d47c41dfe5c50000000002000000000003660000c000000010000000148436e954690b7dbea7d900f78c9f4b0000000004800000a000000010000000484cebe57940ce15323e5b1b75e520f220000000373ab78f30293f8da29923a9eda47351355f2e3f2659ab55c9f61c2342e1c417140000007317556ac035038ac14c55031319b98b27ded63b" | convertto-securestring 
-$ServicePrefix=$StackCode + $DC
+$Domaintojoin = "PME.GBL"
+$OUPath = "OU=TWS,DC=xpme,DC=gbl,DC=msidentity,DC=com"
+$LocalAdminName = "twsadmin"
+$LocalAdminSecuredPassword = ConvertTo-SecureString -AsPlainText "TWS@dm1n#007" -Force
+$ServicePrefix = $StackCode + $DC
 
-if ($OverrideNamingRules -eq $true)
-{
-$NumberofServers=1
+if ($OverrideNamingRules -eq $true) {
+    $NumberofServers = 1
 }
 
 
-Switch ($DC)
-{
-"DC0" 
-{
-#BAY DC
-$XpertTWSEnvName="WindowsStore-Prod-WestUS2" #If Xpert Install these variable need to be paramterised.
-$xpertservicekey="1EE9AE3EF56E8B56EB3E997894AE341B0922CC37D75A8762E6F36D1922EC51EFF6632B0AB59549B9839CF34351FB81CC32396D0987C0C3A78C306E12A71D3D61"
+Switch ($DC) {
+    "DC0" {
+        #BAY DC
+        $XpertTWSEnvName = "WindowsStore-Prod-WestUS2" #If Xpert Install these variable need to be paramterised.
+        $xpertservicekey = "1EE9AE3EF56E8B56EB3E997894AE341B0922CC37D75A8762E6F36D1922EC51EFF6632B0AB59549B9839CF34351FB81CC32396D0987C0C3A78C306E12A71D3D61"
 
-$VirtualNetworkName="TWS-VNET-WUS-PROD"
-$VirtualNetworkResourceGroup="Hypernet-WUS-RG" 
-$MachineSubnetName="Subnet1"
-$VMResourceGroup="TWS-VM-WUS-RG"
-$DCCode="BA3"
+        $VirtualNetworkName = "TWS-VNET-WUS-PROD"
+        $VirtualNetworkResourceGroup = "Hypernet-WUS-RG" 
+        $MachineSubnetName = "Subnet1"
+        $VMResourceGroup = "TWS-VM-WUS-RG"
+        $DCCode = "BA3"
 
-}
-"DC1" 
-{
-#West US2 DC
-$XpertTWSEnvName="WindowsStore-Prod-WestUS2" #If Xpert Install these variable need to be paramterised.
-$xpertservicekey="1EE9AE3EF56E8B56EB3E997894AE341B0922CC37D75A8762E6F36D1922EC51EFF6632B0AB59549B9839CF34351FB81CC32396D0987C0C3A78C306E12A71D3D61"
+    }
+    "DC1" {
+        #West US2 DC
+        $XpertTWSEnvName = "WindowsStore-Prod-WestUS2" #If Xpert Install these variable need to be paramterised.
+        $xpertservicekey = "1EE9AE3EF56E8B56EB3E997894AE341B0922CC37D75A8762E6F36D1922EC51EFF6632B0AB59549B9839CF34351FB81CC32396D0987C0C3A78C306E12A71D3D61"
 
-$VirtualNetworkName="TWS-VNET-WUS2-PROD"
-$VirtualNetworkResourceGroup="Hypernet-WUS2-RG" 
-$MachineSubnetName="Subnet1"
-$VMResourceGroup="TWS-VM-WUS2-RG"
-$DCCode="BY3"
-}
-"DC2" 
-{
+        $VirtualNetworkName = "TWS-VNET-WUS2-PROD"
+        $VirtualNetworkResourceGroup = "Hypernet-WUS2-RG" 
+        $MachineSubnetName = "Subnet1"
+        $VMResourceGroup = "TWS-VM-WUS2-RG"
+        $DCCode = "BY3"
+    }
+    "DC2" {
 
-#East US2 DC
-$XpertTWSEnvName="WindowsStore-Prod-EastUS2"
-$xpertservicekey="1B90F2CF04D7290AB8C7C34C8B521988CB1568659C9E9D57C925E354CE96906DC19DF07D09C828B01695EE6FCA4C07AD59EB6504B90345CA0F93276A0D903DD6"
+        #East US2 DC
+        $XpertTWSEnvName = "WindowsStore-Prod-EastUS2"
+        $xpertservicekey = "1B90F2CF04D7290AB8C7C34C8B521988CB1568659C9E9D57C925E354CE96906DC19DF07D09C828B01695EE6FCA4C07AD59EB6504B90345CA0F93276A0D903DD6"
  
-$VirtualNetworkName="TWS-VNET-EUS2-PROD"
-$VirtualNetworkResourceGroup="Hypernet-EUS2-RG"
-$MachineSubnetName="Subnet1"
-$VMResourceGroup="TWS-VM-EUS2-RG"
-$DCCode="BN2"
-}
+        $VirtualNetworkName = "TWS-VNET-EUS2-PROD"
+        $VirtualNetworkResourceGroup = "Hypernet-EUS2-RG"
+        $MachineSubnetName = "Subnet1"
+        $VMResourceGroup = "TWS-VM-EUS2-RG"
+        $DCCode = "BN2"
+    }
 
-"DC3" 
-{ 
-#North Euprope DC
-$XpertTWSEnvName="WindowsStore-Prod-NorthEurope"
-$xpertservicekey="B3BB776261E4B53997C2E7EF5B3154FB364C544A8640DEACB1C8A37F111E5D321344B989B879CD5EE448943306F50D60E1313EFDC14CC7347BC24AB344F94799"
+    "DC3" { 
+        #North Euprope DC
+        $XpertTWSEnvName = "WindowsStore-Prod-NorthEurope"
+        $xpertservicekey = "B3BB776261E4B53997C2E7EF5B3154FB364C544A8640DEACB1C8A37F111E5D321344B989B879CD5EE448943306F50D60E1313EFDC14CC7347BC24AB344F94799"
 
-$VirtualNetworkName="TWS-VNET-NEUR-PROD"
-$VirtualNetworkResourceGroup="Hypernet-NEUR-RG"
-$MachineSubnetName="Subnet1"
-$VMResourceGroup="TWS-VM-NEUR-RG"
-$DCCode="DB5"
-}
-"POC" 
-{ 
-#BAY DC
-$XpertTWSEnvName="WindowsStore-Prod-WestUS2" #If Xpert Install these variable need to be paramterised.
-$xpertservicekey="1EE9AE3EF56E8B56EB3E997894AE341B0922CC37D75A8762E6F36D1922EC51EFF6632B0AB59549B9839CF34351FB81CC32396D0987C0C3A78C306E12A71D3D61"
+        $VirtualNetworkName = "TWS-VNET-NEUR-PROD"
+        $VirtualNetworkResourceGroup = "Hypernet-NEUR-RG"
+        $MachineSubnetName = "Subnet1"
+        $VMResourceGroup = "TWS-VM-NEUR-RG"
+        $DCCode = "DB5"
+    }
+    "POC" { 
+        #BAY DC
+        $XpertTWSEnvName = "WindowsStore-Prod-WestUS2" #If Xpert Install these variable need to be paramterised.
+        $xpertservicekey = "1EE9AE3EF56E8B56EB3E997894AE341B0922CC37D75A8762E6F36D1922EC51EFF6632B0AB59549B9839CF34351FB81CC32396D0987C0C3A78C306E12A71D3D61"
 
-$VirtualNetworkName="TWSHYPERNET-WUS2-1"
-$VirtualNetworkResourceGroup="HypernetWUS2RG"
-$MachineSubnetName="Subnet1"
-$VMResourceGroup="UST-TWS-PROD-RG"
-$DCCode="MWH"
-}
+        $VirtualNetworkName = "TWSHYPERNET-WUS2-1"
+        $VirtualNetworkResourceGroup = "HypernetWUS2RG"
+        $MachineSubnetName = "Subnet1"
+        $VMResourceGroup = "UST-TWS-PROD-RG"
+        $DCCode = "MWH"
+    }
 
 }
 
-Switch ($StackCode)
-{
-"WPW"
-{
-$XpertRoleName="AntiPiracyStack"
-}
-"WDW"
-{
-$XpertRoleName="AppCatalogReviewStack"
-}
-"BPB"
-{
-$XpertRoleName="BackEndPipeline"
-}
-"WIW"
-{
-$XpertRoleName="ClientInputStack"
-}
-"WTW"
-{
-$XpertRoleName="ComTransactionLicenseStack"
-}
-"WAW"
-{
-$XpertRoleName="ConfigStackPrimary"
-}
-"OCO"
-{
-$XpertRoleName="ContentOrigin"
-}
-"WSW"
-{
-$XpertRoleName="DataPresentationStack"
-}
-"WQW"
-{
-$XpertRoleName="EventQueueStack"
-}
-"WCW"
-{
-$XpertRoleName="IdentityCatalogConsumerStack"
-}
+Switch ($StackCode) {
+    "WPW" {
+        $XpertRoleName = "AntiPiracyStack"
+    }
+    "WDW" {
+        $XpertRoleName = "AppCatalogReviewStack"
+    }
+    "BPB" {
+        $XpertRoleName = "BackEndPipeline"
+    }
+    "WIW" {
+        $XpertRoleName = "ClientInputStack"
+    }
+    "WTW" {
+        $XpertRoleName = "ComTransactionLicenseStack"
+    }
+    "WAW" {
+        $XpertRoleName = "ConfigStackPrimary"
+    }
+    "OCO" {
+        $XpertRoleName = "ContentOrigin"
+    }
+    "WSW" {
+        $XpertRoleName = "DataPresentationStack"
+    }
+    "WQW" {
+        $XpertRoleName = "EventQueueStack"
+    }
+    "WCW" {
+        $XpertRoleName = "IdentityCatalogConsumerStack"
+    }
 
-"WLW"
-{
-$XpertRoleName="LPS"
-}
+    "WLW" {
+        $XpertRoleName = "LPS"
+    }
 
-"WCW"
-{
-$XpertRoleName="PipelineStack"
-}
+    "WCW" {
+        $XpertRoleName = "PipelineStack"
+    }
 
-"DFP"
-{
-$XpertRoleName="DevPortalFile"
-}
+    "DFP" {
+        $XpertRoleName = "DevPortalFile"
+    }
 
-"ACP"
-{
-$XpertRoleName="PortalAppFabricCache"
-}
+    "ACP" {
+        $XpertRoleName = "PortalAppFabricCache"
+    }
 
-"WPP"
-{
-$XpertRoleName="PortalDetailsStack"
-}
+    "WPP" {
+        $XpertRoleName = "PortalDetailsStack"
+    }
 
-"PRP"
-{
-$XpertRoleName="PortalFrontEndWeb"
-}
+    "PRP" {
+        $XpertRoleName = "PortalFrontEndWeb"
+    }
 
 
-"WSP"
-{
-$XpertRoleName="PortalStack"
-}
+    "WSP" {
+        $XpertRoleName = "PortalStack"
+    }
 
-"SLR"
-{
-$XpertRoleName="RemoteSigningFrontEndWeb"
-}
+    "SLR" {
+        $XpertRoleName = "RemoteSigningFrontEndWeb"
+    }
 
-"ACS"
-{
-$XpertRoleName="ServicesAppFabricCache"
-}
+    "ACS" {
+        $XpertRoleName = "ServicesAppFabricCache"
+    }
 
-"AIS"
-{
-$XpertRoleName="ServicesAppFabricCache"
-}
+    "AIS" {
+        $XpertRoleName = "ServicesAppFabricCache"
+    }
 
-"SMS"
-{
-$XpertRoleName="ServicesFESQLPipelinePrincipal"
-}
+    "SMS" {
+        $XpertRoleName = "ServicesFESQLPipelinePrincipal"
+    }
 
-"SRS"
-{
-$XpertRoleName="ServicesFrontEndWeb"
-}
+    "SRS" {
+        $XpertRoleName = "ServicesFrontEndWeb"
+    }
 
-"PAT"
-{
-$XpertRoleName="PatchingandTools"
-}
+    "PAT" {
+        $XpertRoleName = "PatchingandTools"
+    }
 
-"WAP"
-{
-$XpertRoleName="PortalAuditStack"
-}
+    "WAP" {
+        $XpertRoleName = "PortalAuditStack"
+    }
 
-"GFB"
-{
-$XpertRoleName="GeneralPurposeStorage"
-}
+    "GFB" {
+        $XpertRoleName = "GeneralPurposeStorage"
+    }
 
-"UTL"
-{
-$XpertRoleName="PatchingandTools"
-}
+    "UTL" {
+        $XpertRoleName = "PatchingandTools"
+    }
 
-default
-{
-$XpertRoleName="UCProdServer"
-}
+    default {
+        $XpertRoleName = "UCProdServer"
+    }
 
 }
 
-if ($DC -eq "POC")
-{
-$XpertRoleName="POCMachines"
+if ($DC -eq "POC") {
+    $XpertRoleName = "POCMachines"
 }
 
-for ($ctr=1; $ctr -le $NumberofServers; $ctr++)
-{
-if ($OverrideNamingRules -eq $true)
-{
-Write-Host "Print Hello"
-$vmname=$ServerName
-}
-else
-{
-$vmname=$DCCode + "TWS" + $SubRole + $StackCode + $StartCounter
-}
-$StartCounter++
-$DeploymentName=$vmname + "_Deployment"
+for ($ctr = 1; $ctr -le $NumberofServers; $ctr++) {
+    if ($OverrideNamingRules -eq $true) {
+        Write-Host "Print Hello"
+        $vmname = $ServerName
+    }
+    else {
+        $vmname = $DCCode + "TWS" + $SubRole + $StackCode + $StartCounter
+    }
+    $StartCounter++
+    $DeploymentName = $vmname + "_Deployment"
 
-Write-Host "Parameters: -Name $DeploymentName -ResourceGroupName $VMResourceGroup -TemplateFile $templatepath -VMName $vmname -SizeofOSDiskInGB $CDrive -Disk1-D-Drive $DDrive -Disk2-E-Drive $EDrive -Disk3-H-Drive $HDrive -Disk4-O-Drive $ODrive -Disk5-T-Drive $TDrive -ManagedDiskStorageType $StorageType -VmSize $SKU -ServicePrefix $ServicePrefix -AdminUserName $LocalAdminName -AdminPassword $LocalAdminSecuredPassword -OUPath $OUPath -MachineSubnetName $MachineSubnetName -VirtualNetworkResourceGroup $VirtualNetworkResourceGroup -VirtualNetworkName $VirtualNetworkName -Domaintojoin $Domaintojoin  -DomainUsername $DomainAccountName -DomainPassword $DomainPassword  -XpertEnvName $XpertTWSEnvName -XpertRole $XpertRoleName -XpertServiceKey $xpertservicekey -AsJob"
+    Write-Host "Parameters: -Name $DeploymentName -ResourceGroupName $VMResourceGroup -TemplateFile $templatepath -VMName $vmname -SizeofOSDiskInGB $CDrive -Disk1-D-Drive $DDrive -Disk2-E-Drive $EDrive -Disk3-H-Drive $HDrive -Disk4-O-Drive $ODrive -Disk5-T-Drive $TDrive -ManagedDiskStorageType $StorageType -VmSize $SKU -ServicePrefix $ServicePrefix -AdminUserName $LocalAdminName -AdminPassword $LocalAdminSecuredPassword -OUPath $OUPath -MachineSubnetName $MachineSubnetName -VirtualNetworkResourceGroup $VirtualNetworkResourceGroup -VirtualNetworkName $VirtualNetworkName -Domaintojoin $Domaintojoin  -DomainUsername $DomainAccountName -DomainPassword $DomainPassword  -XpertEnvName $XpertTWSEnvName -XpertRole $XpertRoleName -XpertServiceKey $xpertservicekey -AsJob"
 
-Switch ($Role)
-{
+    Switch ($Role) {
 
-"SQL"
-{
-$templatepath=$sqlTemplate
-New-AzureRmResourceGroupDeployment -Name $DeploymentName -ResourceGroupName $VMResourceGroup -TemplateFile $templatepath -VMName $vmname -SizeofOSDiskInGB $CDrive -Disk1-D-Drive $DDrive -Disk2-E-Drive $EDrive -Disk3-H-Drive $HDrive -Disk4-O-Drive $ODrive -Disk5-T-Drive $TDrive -ManagedDiskStorageType $StorageType -VmSize $SKU -ServicePrefix $ServicePrefix -AdminUserName $LocalAdminName -AdminPassword $LocalAdminSecuredPassword -OUPath $OUPath -MachineSubnetName $MachineSubnetName -VirtualNetworkResourceGroup $VirtualNetworkResourceGroup -VirtualNetworkName $VirtualNetworkName -Domaintojoin $Domaintojoin  -DomainUsername $DomainAccountName -DomainPassword $DomainPassword -XpertEnvName $XpertTWSEnvName -XpertRole $XpertRoleName -XpertServiceKey $xpertservicekey  -AsJob
-}
-"Web"
-{
-$templatepath=$webTemplate
-New-AzureRmResourceGroupDeployment -Name $DeploymentName -ResourceGroupName $VMResourceGroup -TemplateFile $templatepath -VMName $vmname -SizeofOSDiskInGB $CDrive -Disk1-D-Drive $DDrive -Disk2-E-Drive $EDrive -ManagedDiskStorageType $StorageType -VmSize $SKU -ServicePrefix $ServicePrefix -AdminUserName $LocalAdminName -AdminPassword $LocalAdminSecuredPassword -OUPath $OUPath -MachineSubnetName $MachineSubnetName -VirtualNetworkResourceGroup $VirtualNetworkResourceGroup -VirtualNetworkName $VirtualNetworkName -Domaintojoin $Domaintojoin  -DomainUsername $DomainAccountName -DomainPassword $DomainPassword -XpertEnvName $XpertTWSEnvName -XpertRole $XpertRoleName -XpertServiceKey $xpertservicekey -AsJob
-}
-}
+        "SQL" {
+            $templatepath = $sqlTemplate
+            New-AzureRmResourceGroupDeployment -Name $DeploymentName -ResourceGroupName $VMResourceGroup -TemplateFile $templatepath -VMName $vmname -SizeofOSDiskInGB $CDrive -Disk1-D-Drive $DDrive -Disk2-E-Drive $EDrive -Disk3-H-Drive $HDrive -Disk4-O-Drive $ODrive -Disk5-T-Drive $TDrive -ManagedDiskStorageType $StorageType -VmSize $SKU -ServicePrefix $ServicePrefix -AdminUserName $LocalAdminName -AdminPassword $LocalAdminSecuredPassword -OUPath $OUPath -MachineSubnetName $MachineSubnetName -VirtualNetworkResourceGroup $VirtualNetworkResourceGroup -VirtualNetworkName $VirtualNetworkName -Domaintojoin $Domaintojoin  -DomainUsername $DomainAccountName -DomainPassword $DomainPassword -XpertEnvName $XpertTWSEnvName -XpertRole $XpertRoleName -XpertServiceKey $xpertservicekey  -AsJob
+        }
+        "Web" {
+            $templatepath = $webTemplate
+            New-AzureRmResourceGroupDeployment -Name $DeploymentName -ResourceGroupName $VMResourceGroup -TemplateFile $templatepath -VMName $vmname -SizeofOSDiskInGB $CDrive -Disk1-D-Drive $DDrive -Disk2-E-Drive $EDrive -ManagedDiskStorageType $StorageType -VmSize $SKU -ServicePrefix $ServicePrefix -AdminUserName $LocalAdminName -AdminPassword $LocalAdminSecuredPassword -OUPath $OUPath -MachineSubnetName $MachineSubnetName -VirtualNetworkResourceGroup $VirtualNetworkResourceGroup -VirtualNetworkName $VirtualNetworkName -Domaintojoin $Domaintojoin  -DomainUsername $DomainAccountName -DomainPassword $DomainPassword -XpertEnvName $XpertTWSEnvName -XpertRole $XpertRoleName -XpertServiceKey $xpertservicekey -AsJob
+        }
+    }
 
 }
 
