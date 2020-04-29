f02e783a9dae523844fc138375834c724f7b8ff7
SrikanthMargam <64267843+SrikanthMargam@users.noreply.github.com>
2020-04-29 15:39:33 +0530

    Update PostDeploy.ps1
    Modified to sample

--- TWSInfra/SupportFiles/PostDeploy.ps1
+++ TWSInfra/SupportFiles/PostDeploy.ps1
@@ -1,4 +1,3 @@
-﻿
 param (
     [string]$Username,
     [string]$Password,
@@ -6,33 +5,11 @@ param (
     [string]$xpertenvName,
     [string]$xpertRole,
     [string]$xpertServiceKey
-
 )
-function Write-FileLog
-{
-param
-(
-[string] $message
-)
-$logfilename="C:\WindowsAzure\Logs\TWSCustomInstallLog.log"
-Write-Output $message | Out-File -FilePath $logfilename -Append -Force
-}
 
-Write-FileLog("Downloading SupportFiles.Zip...")
-RoboCopy "\\BY3TWSWEBUTL101\ScriptsandExecutables" C:\Packages\Plugins SupportFiles.zip /XN
-
-Sleep 5
-Write-FileLog("Extracting SupportFiles.Zip to C:\Packages\Plugins...")
-Expand-Archive C:\Packages\Plugins\SupportFiles.zip -DestinationPath C:\Packages\Plugins\SupportFiles
-
-Write-FileLog("Converting $UserName and password to Credential...")
-$pass = ConvertTo-SecureString -AsPlainText $Password -Force
-
-$SecureString = $pass
-# Users you password securly
-$MySecureCreds = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $Username,$SecureString
-
-Write-FileLog("Calling AppConfiguration.ps1...")
-Invoke-Command -ComputerName $env:COMPUTERNAME -Credential $MySecureCreds -FilePath 'C:\Packages\Plugins\SUpportFiles\AppConfiguration.ps1' -ArgumentList $Role,$xpertenvName,$xpertRole,$xpertServiceKey
-Write-FileLog("Completed Post Deploy Activities...")
-
+Write-Host $Username
+Write-Host $Password
+Write-Host $Role
+Write-Host $xpertenvName
+Write-Host $xpertRole
+Write-Host $xpertServiceKey 
